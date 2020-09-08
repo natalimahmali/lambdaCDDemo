d9e7df406bf9577d995f5c09986aa7a38a983f55
Natali <nataliemah67@gmail.com>
2020-09-08 15:55:26 +0300

    changes

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,7 +1,6 @@
 import boto3
 import json
 
-ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
+    response = "Hello from BioBeat's first CICD Lambda function!"
     return {"statusCode": 200, "body": json.dumps(response)}
